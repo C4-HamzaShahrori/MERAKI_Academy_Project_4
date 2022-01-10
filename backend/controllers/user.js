23b667b838a13eceab1d02cecfd12df4b18621f8
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-10 21:19:37 +0200

    addDoctorToCategory

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,39 +1,43 @@
- const user = require("../database/models/user")
-const usersModel=require("../database/models/user")
+const usersModel = require("../database/models/user");
 
+const createNewUser = (req, res) => {
+  const { firstName, lastName, age, country, email, password, role } = req.body;
+  const newUser = new usersModel({
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+    role,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      res
+        .status(201)
+        .json({
+          success: true,
+          message: `Success of the registration process`,
+          user: result,
+        });
+    })
+    .catch((err) => {
+      //  console.log(err.kind);
+      if (err.keyPattern) {
+        return res.status(409).json({
+          success: false,
+          message: `The email already exists`,
+        });
+      }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err,
+      });
+    });
+};
 
-
- const createNewUser=(req,res)=>{
-     const{firstName, lastName, age, country, email, password, role}=req.body
-     const newUser=new usersModel ({
-        firstName,
-        lastName,
-        age,
-        country,
-        email,
-        password,
-        role,
-     })
-     newUser.save().then((result)=>{
-         res.status(201).json({success:true,
-            message:`Success of the registration process`,
-            user:result
-        })
-     }).catch((err)=>{
-         if(err.keyPattern){
-             return res.status(409).json({
-                success: false,
-                message: `The email already exists`,
-             })
-         }
-         res.status(500).json({
-            success: false,
-            message: `Server Error`,
-            err: err,
-          });
-     })
- }
-
- module.exports={
-    createNewUser
- }
+module.exports = {
+  createNewUser,
+};
