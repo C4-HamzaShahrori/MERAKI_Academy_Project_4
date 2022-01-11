3dc7565b85b111f01d3a9fded44283c817d7a28d
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-11 12:58:56 +0200

    authentication

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,14 @@
+const jwt =require("jsonwebtoken")
+const SECRET=process.env.SECRET
+
+const authentication =(req,res,next)=>{
+    let token =req.headers.authorization
+    if(typeof token !== "undefined"){
+        token =req.headers.authorization.split(" ")[1]
+        jwt.verify(token,SECRET,(err,result)=>{
+            if(!result){res.status(403).json({ success: false, message: `The token is invalid or expired` })}
+            else{next()}
+        })
+    }
+    else{res.status(403).json({ success: false, message: "Forbidden" })}
+}
