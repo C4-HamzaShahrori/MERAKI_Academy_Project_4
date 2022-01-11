27de2f00d2ebd6745646b20f7d535848f3c78826
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-11 21:27:36 +0200

    createNewComment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,5 +1,41 @@
-const commentModel=require("../database/models/comment")
+const commentModel = require("../database/models/comment");
 
-const doctorModel=require("../database/models/doctor")
+const doctorModel = require("../database/models/doctor");
 
+const addCommentToDoctor = (req, res) => {
+  const doctorId = req.params.id;
+  const { comment } = req.body;
+  const newComment = new commentModel({
+    comment,
+     commenter: req.token.userId,
+  });
+  newComment
+    .save()
+    .then((result) => {
+      doctorModel
+        .updateOne({ _id: doctorId }, { $push: { comment: result} })
+        .then(() => {
+          res.status(201).json({
+            success: true,
+            message: `The new comment added`,
+            comment: result,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            success: false,
+            message: `Server Error`,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    });
+};
 
+module.exports = {
+  addCommentToDoctor,
+};
