57d7cf1e278182bcca507b3cb0a023bfa3a59af6
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-11 12:57:34 +0200

    getAllDoctors

--- backend/controllers/doctor.js
+++ backend/controllers/doctor.js
@@ -1,7 +1,9 @@
 const doctorModel = require("../database/models/doctor");
 
+
+
 const addDoctorToCategory = (req, res) => {
-  const { firstName, lastName, age, numberPhone, address, comment } = req.body;
+  const { firstName, lastName, age, numberPhone, address, comment ,specialized} = req.body;
   const newDoctor = new doctorModel({
     firstName,
     lastName,
@@ -9,6 +11,7 @@ const addDoctorToCategory = (req, res) => {
     numberPhone,
     address,
     comment,
+    specialized
   });
   newDoctor
     .save()
@@ -26,6 +29,42 @@ const addDoctorToCategory = (req, res) => {
     });
 };
 
+
+const getAllDoctors = (req,res)=>{
+    doctorModel.find({}).then((result)=>{
+        if (!result[0]){
+            res.status(200).json(`No result`)
+        }else{res.status(200).json({
+            success:true,
+            message:`all doctor`,
+            result:result
+        })}
+    }).catch((err)=>{
+        res.sendStatus(500)
+    })
+}
+
+
+ const getAllDoctorsBySpecialty=(req,res)=>{
+     const specialized=req.query.specialized
+    doctorModel.find({specialized:specialized}).then((result)=>{
+        if(!result[0]){
+            res.status(200).json(`No result`)
+        }else {res.status(200).json({
+            success:true,
+            message:`all doctor by specialized `,
+            result:result
+        })}
+
+    }).catch((err)=>{
+        res.sendStatus(500)
+    })
+ }
+
+
+
 module.exports = {
   addDoctorToCategory,
+  getAllDoctors,
+  getAllDoctorsBySpecialty
 };
