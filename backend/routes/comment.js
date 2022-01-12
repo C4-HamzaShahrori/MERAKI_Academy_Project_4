c4f3f0b21df17b2e87557e9fe507b02a533a5496
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-12 13:17:59 +0200

    getAllComment and deleteCommentById

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -5,8 +5,16 @@ const commentRouter=express.Router()
 const {authentication}=require("../middleware/authentication")
 const{authorization}=require("../middleware/authorization")
 
-const {addCommentToDoctor}=require("../controllers/comment")
+const {addCommentToDoctor,updateCommentOnDoctor,deleteCommentById,getAllComments}=require("../controllers/comment")
 
 commentRouter.post("/:id",authentication,authorization("create-comment"),addCommentToDoctor)
+commentRouter.put("/:id",authentication,authorization("create-comment"),updateCommentOnDoctor)
+commentRouter.delete("/:id",authentication,authorization("create-comment"),deleteCommentById)
+
+commentRouter.get("/",authentication,authorization("create-comment"),getAllComments)
+
+
+
+
 
 module.exports=commentRouter
