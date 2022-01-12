aab2bb54ad3ecf778aff59e0efb46a517b8813e3
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-12 13:16:19 +0200

    updateCommentById

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,19 +1,20 @@
 const commentModel = require("../database/models/comment");
 
 const doctorModel = require("../database/models/doctor");
+const userModel = require("../database/models/user");
 
 const addCommentToDoctor = (req, res) => {
   const doctorId = req.params.id;
   const { comment } = req.body;
   const newComment = new commentModel({
     comment,
-     commenter: req.token.userId,
+    commenter: req.token.userId,
   });
   newComment
     .save()
     .then((result) => {
       doctorModel
-        .updateOne({ _id: doctorId }, { $push: { comment: result} })
+        .updateOne({ _id: doctorId }, { $push: { comment: result } })
         .then(() => {
           res.status(201).json({
             success: true,
@@ -36,8 +37,80 @@ const addCommentToDoctor = (req, res) => {
     });
 };
 
+const updateCommentOnDoctor = (req, res) => {
+  const userId = req.body.userId;
+  const idComment = req.params.id;
+  
+ 
+  commentModel.findById(idComment).then((result) => {
+    console.log(result.commenter);
 
+    if (result.commenter == userId) {
+      commentModel
+        .findByIdAndUpdate(idComment, req.body, { new: true })
+        .populate("commenter", "firstName -_id")
+        .then((resultAfterUpdate) => {
+          res.status(202).json({
+            success: true,
+            message: `The comment updated`,
+            result: resultAfterUpdate,
+          });
+        })
+        .catch((err) => {
+          res.sendStatus(500);
+        });
+    } else {
+      res
+        .status(406)
+        .json({ success: false, message: `con't update the comment ` });
+    }
+  });
+};
+
+const deleteCommentById = (req, res) => {
+  const idComment = req.params.id;
+  const userId = req.body.userId;
+  commentModel.findById(idComment).then((result) => {
+  
+    if (result.commenter == userId) {
+      commentModel
+        .findByIdAndDelete(idComment)
+        .then((resultAfterDelete) => {
+          res.status(200).json({
+            success: true,
+            message: ` delete the comment `,
+            result: resultAfterDelete,
+          });
+        })
+        .catch((err) => {
+          res.sendStatus(500);
+        });
+    } else {
+      res.status(406).json({ success: false, message: `con't delete comment` });
+    }
+  });
+};
+
+const getAllComments = (req, res) => {
+  commentModel
+    .find({})
+    .then((result) => {
+      if (!result[0]) {
+        res.status(200).json(`No result`);
+      } else {
+        res
+          .status(200)
+          .json({ success: true, message: `all comment`, result: result });
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+};
 
 module.exports = {
   addCommentToDoctor,
+  updateCommentOnDoctor,
+  deleteCommentById,
+  getAllComments,
 };
