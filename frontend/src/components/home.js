081ccf6f4c9a87af15187e79e73243141eb65b4c
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-13 09:47:51 +0200

    component home and modfied css

---
+++ frontend/src/components/home.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react"
+import axios from "axios"
+
+
+
+const Home =()=>{
+return(<div className="home" >
+<div className="divDefinition" >
+<p id="definition">The right path to the right doctor  </p>
+<button id="signUp" >Sign up</button>
+<button id="signIn" >Sign in</button>
+</div>
+</div>)
+
+}
+
+
+export default Home
+
+
