3fd7b4929e9029994f1327edcf44da47a468fee6
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-16 12:12:05 +0200

    add new butoton in home bage (Skip)

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,14 +1,15 @@
 import React,{useState} from "react"
 import axios from "axios"
-
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 
 const Home =()=>{
 return(<div className="home" >
 <div className="divDefinition" >
 <p id="definition">The right path to the right doctor  </p>
-<button id="signUp" >Sign up</button>
-<button id="signIn" >Sign in</button>
+<Link to="/signUp"><button id="signUp" >Sign up</button></Link>
+<Link to="/signIn"><button id="signIn" >Sign in</button></Link>
+<Link to="/Skip"><button id="Skip" >Skip</button></Link>
 </div>
 </div>)
 
