7a6bb5db6aa793507a05eb2012a9c5686f072b54
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-16 23:35:21 +0200

    modified Skip.js and added NavBar

--- frontend/src/components/Skip.js
+++ frontend/src/components/Skip.js
@@ -1,70 +1,171 @@
-import React,{useState,useEffect} from "react"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 // import { all } from "../../../backend/routes/recommendedDr";
 
+const Skip = ({ isLogged, token }) => {
+  const [allDoctor, setAllDoctor] = useState("");
+  const [noResult, setNoResult] = useState("");
+  const [comment, setComment] = useState("");
+localStorage.getItem("Token")
+  const getAllDoctors = async () => {
+    try {
+      const result = await axios.get(`http://localhost:5000/doctors`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
+      if (result.data == `No result`) {
+        setNoResult(result.data);
+      } else {
+        console.log(result.data.result);
+        setAllDoctor(result.data.result);
+      }
+    } catch {}
+  };
 
-const Skip=({isLogged})=>{
-const[allDoctor,setAllDoctor]=useState("")
-const[noResult,setNoResult]=useState("")
-useEffect(()=>{
-    getAllDoctors()
-},[])
-const getAllDoctors=()=>{
-    axios.get(`http://localhost:5000/doctors`).then((result)=>
-    {;
-        if (result.data==`No result`){
-            
-        setNoResult(result.data)
-    }else{
-       
-        setAllDoctor(result.data.result)
-       
+  const addComment = async (id) => {
+    try {
+      axios.post(
+        `http://localhost:5000/comment/${id}`,
+        { comment },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      getAllDoctors();
+    } catch (error) {
+      console.log(error.response);
     }
-}).catch((err)=>{
-    
-})
-}
+  };
 
-return(<>
-{isLogged?(  <div className="skipAllDoctors">
-       <div>{allDoctor ?(allDoctor.map((element,index)=>(<div key={index}>
-        <img id="imageDoctor" src={element.image}></img><p>
-           specialized:{element.specialized}
-        <br/>
-           FirstName:{element.firstName}
-           <br/>
-           LastName:{element.lastName}
-           <br/>
-           Address:{element.address}
-           <br/>
-           NumberPhone:{element.numberPhone}
-           <br/>
-         comment:{element.comment}
-       </p>
-     </div>))):(<div>{noResult}</div>)}</div> 
-    </div>):(  <div className="skipAllDoctors">
-       <div>{allDoctor ?(allDoctor.map((element,index)=>(<div key={index}>
-           <img id="imageDoctor" src={element.image}></img>
-           <p>
-           specialized:{element.specialized}
-        <br/>
-           FirstName:{element.firstName}
-           <br/>
-           LastName:{element.lastName}
-           <br/>
-           Address:{element.address}
-           <br/>
-           NumberPhone:{element.numberPhone}
-           <br/>
-       </p>
-     </div>))):(<div>{noResult}</div>)}</div> 
-    </div>)}
-  
-</>
-)
+  useEffect(() => {
+    getAllDoctors();
+  }, []);
+  return (
+    <>
+      {token ? (
+        <div className="skipAllDoctors">
+          <div>
+            {allDoctor ? (
+              allDoctor.map((element, index) => (
+                <div key={index}>
+                  <div>
+                    {" "}
+                    <img id="imageDoctor" src={element.image}></img>
+                  </div>
+                  <div>
+                    {" "}
+                    <p>
+                      specialized:{element.specialized}
+                      <br />
+                      FirstName:{element.firstName}
+                      <br />
+                      LastName:{element.lastName}
+                      <br />
+                      Address:{element.address}
+                      <br />
+                      NumberPhone:{element.numberPhone}
+                      <br />
+                    </p>
+                  </div>
+                  <div>
+                    {element.comment ? (
+                      element.comment.map((comment, index) => {
+                        return (
+                          <p className="comment" key={index}>
+                            {comment.comment}
+                          </p>
+                        );
+                      })
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                  {
+                    <div>
+                      <textarea
+                        className="comment"
+                        placeholder="comment..."
+                        onChange={(e) => {
+                          setComment(e.target.value);
+                        }}
+                      />
+                      <button
+                        className="commentButton"
+                        onClick={() => {
+                          addComment(element._id);
+                        }}
+                      >
+                        Add comment
+                      </button>
+                    </div>
+                  }
+                </div>
+              ))
+            ) : (
+              <div>{noResult}</div>
+            )}
+          </div>
+        </div>
+      ) : (<>
+       
+     <br></br>
+        <div className="skipAllDoctors">
+        
+            {/* <div className="NavSkip" ><button id="Home">Home</button>
+            <button id="SignIn">SignIn</button>
+            <button id="SignUp">SignUp</button></div> */}
+          <div>
+            {allDoctor ? (
+              allDoctor.map((element, index) => (
+                <div key={index}>
+                  <div>
+                    {" "}
+                    <img id="imageDoctor" src={element.image}></img>
+                  </div>
+                  <div>
+                    {" "}
+                    <p>
+                      specialized:{element.specialized}
+                      <br />
+                      FirstName:{element.firstName}
+                      <br />
+                      LastName:{element.lastName}
+                      <br />
+                      Address:{element.address}
+                      <br />
+                      NumberPhone:{element.numberPhone}
+                      <br />
+                    </p>
+                  </div>
+                  <div>
+                    {element.comment ? (
+                      element.comment.map((comment, index) => {
+                        return (
+                          <p className="comment" key={index}>
+                            {comment.comment}
+                          </p>
+                        );
+                      })
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div>{noResult}</div>
+            )}
+          </div>
+        </div>
+        </>
+      )}
+    </>
+  );
+};
 
-}
-
-export default Skip
+export default Skip;
