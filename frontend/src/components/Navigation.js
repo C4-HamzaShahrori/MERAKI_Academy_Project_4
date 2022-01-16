eb06423084c02793b2b5a3e5f6d7aa76005fc8ba
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-16 23:35:54 +0200

    NavBar CSS

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,13 +1,35 @@
 import React,{useState} from "react"
 
 
-import {Link} from "react-router-dom"
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 
 
-const Navigation=()=>{
+const Navigation=({isLogged,userId,setIsLogged,setToken})=>{
+  const navigate=useNavigate()
+  // const[allUser,setAllUser]=useState("")
+  // const getUserById=()=>{
+
+  // }
+
+  const logout = () => {
+    setIsLogged(false);
+    localStorage.clear();
+    setToken("")
+    navigate("/signIn")
+  };
     return(
-      <div>
-          <h2></h2>
-      </div>
+      <>
+      <div className="NavigationBar">
+        {isLogged?(<>   <a id="Home" href="/">Home</a>
+        <a onClick={logout} id="SignIn" >Logout</a>
+       <a  id="SignUp" href="/signUp">SignUp</a></>):(<>   <a id="Home" href="/">Home</a>
+        <a id="SignIn" href="/signIn">SignIn</a>
+       <a id="SignUp" href="/signUp">SignUp</a></>)}
+   
+    </div>
+    </>
     )
 }
+
+
+export default Navigation
