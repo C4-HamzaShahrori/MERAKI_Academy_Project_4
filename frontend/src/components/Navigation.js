949b3bc28ed609c43e425a27c497259ded58325b
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-17 13:06:54 +0200

    modfied navBar to show userName in nav bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,35 +1,74 @@
-import React,{useState} from "react"
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 
+const Navigation = ({
+  isLogged,
+  userId,
+  setIsLogged,
+  setToken,
+  userLastName,
+  userFirstName,
+  setUserLastName,
+  setUserFirstName,
+}) => {
+  const navigate = useNavigate();
 
-const Navigation=({isLogged,userId,setIsLogged,setToken})=>{
-  const navigate=useNavigate()
-  // const[allUser,setAllUser]=useState("")
-  // const getUserById=()=>{
+  const getUserById = async () => {
+    try {
+      const result = await axios.get(`http://localhost:5000/users/${userId}`);
+      // console.log(result.data.user.firstName);
+      setUserFirstName(result.data.user.firstName);
+      setUserLastName(result.data.user.lastName);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
-  // }
+  useEffect(() => {
+    getUserById();
+  }, [userId]);
 
   const logout = () => {
     setIsLogged(false);
     localStorage.clear();
-    setToken("")
-    navigate("/signIn")
+    setToken("");
+    navigate("/signIn");
   };
-    return(
-      <>
+  return (
+    <>
       <div className="NavigationBar">
-        {isLogged?(<>   <a id="Home" href="/">Home</a>
-        <a onClick={logout} id="SignIn" >Logout</a>
-       <a  id="SignUp" href="/signUp">SignUp</a></>):(<>   <a id="Home" href="/">Home</a>
-        <a id="SignIn" href="/signIn">SignIn</a>
-       <a id="SignUp" href="/signUp">SignUp</a></>)}
-   
-    </div>
+        {isLogged ? (
+          <>
+            {" "}
+            <a id="Home" href="/">
+              Home
+            </a>
+            <a onClick={logout} id="SignIn">
+              Logout
+            </a>
+            <a id="SignUp" href="#">
+              {userFirstName} {userLastName}
+            </a>
+          </>
+        ) : (
+          <>
+            {" "}
+            <a id="Home" href="/">
+              Home
+            </a>
+            <a id="SignIn" href="/signIn">
+              SignIn
+            </a>
+            <a id="SignUp" href="/signUp">
+              SignUp
+            </a>
+          </>
+        )}
+      </div>
     </>
-    )
-}
+  );
+};
 
-
-export default Navigation
+export default Navigation;
