7ff4c2fbce5119a25a29e15ec5d236f128c0531b
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-01-17 13:03:46 +0200

    modified App.js and create useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,27 @@ import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import Skip from "./components/Skip.js"
-import Navigation from "./components/Navigation"
+import Skip from "./components/Skip/Skip.js";
+import Navigation from "./components/Navigation";
 
 function App() {
-  const[token,setToken]=useState("")
-const [isLogged,setIsLogged]=useState(false)
-const [userId,setUserId]=useState("")
+  const [token, setToken] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [userFirstName, setUserFirstName] = useState("");
+  const [userLastName, setUserLastName] = useState("");
   return (
     <>
-    <Navigation isLogged={isLogged} userId={userId}setToken={setToken} setIsLogged={setIsLogged} />
+      <Navigation
+        isLogged={isLogged}
+        userId={userId}
+        setToken={setToken}
+        setIsLogged={setIsLogged}
+        userFirstName={userFirstName}
+        userLastName={userLastName}
+        setUserFirstName={setUserFirstName}
+        setUserLastName={setUserLastName}
+      />
       <div className="App">
         <h1>healthApp</h1>
       </div>
@@ -21,8 +32,24 @@ const [userId,setUserId]=useState("")
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signUp" element={<SignUp />} />
-        <Route path="/signIn" element={<SignIn setToken={setToken} setIsLogged={setIsLogged} token={token} setUserId={setUserId}/>} />
-        <Route path="/AllDoctor" element={<Skip isLogged={isLogged} token={token}/>} />
+        <Route
+          path="/signIn"
+          element={
+            <SignIn
+              setUserFirstName={setUserFirstName}
+              setUserLastName={setUserLastName}
+              userId={userId}
+              setToken={setToken}
+              setIsLogged={setIsLogged}
+              token={token}
+              setUserId={setUserId}
+            />
+          }
+        />
+        <Route
+          path="/AllDoctor"
+          element={<Skip isLogged={isLogged} token={token} />}
+        />
       </Routes>
     </>
   );
